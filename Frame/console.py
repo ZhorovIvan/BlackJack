c4ff07249b3692898bec390d632d5812fd21840c
ZhorovIvan <jorov.and@yandex.ru>
2021-10-26 22:41:48 +0600

    finish work in the main class

--- Frame/console.py
+++ Frame/console.py
@@ -8,36 +8,41 @@ class Desk:
     ace = "Ace"
 
     def __init__(self):
-        self.human_cards = []
-        self.amount_humon_cards = 0
+        self.cards = []
+        self.amount_cards = 0
 
 
     def add_amount(self, card):
         match card:
             case self.jack:
-                self.amount_humon_cards += 2
+                self.amount_cards += 2
             case self.queen:
-                self.amount_humon_cards += 3
+                self.amount_cards += 3
             case self.king:
-                self.amount_humon_cards += 4
+                self.amount_cards += 4
             case self.ace:
-                self.amount_humon_cards += 11
+                self.amount_cards += 11
             case _:
-                self.amount_humon_cards += int(card)
+                self.amount_cards += int(card)
 
 
     def has_too_many_cards(self):
-        return self.amount_humon_cards > 21
-
-
-    def print_all_cards(self):
-        return (str(self.amount_humon_cards))
-
-
-    def clear_desk(self):
-        os.system('cls' if os.name == 'nt' else 'clear')
+        return self.amount_cards > 21
 
 
     def add_card(self,card):
-            self.human_cards.append(card)
-            self.add_amount(card[0])
+        self.cards.append(card)
+        self.add_amount(card[0])
+
+
+    def get_all_cards(self, who_is_moved):
+        return who_is_moved + " cards - " + str(self.cards).replace("[", "").replace("]", "") +\
+                "\nSum " + who_is_moved + " cards equals - " + str(self.amount_cards)
+
+
+    def is_enouth(self):
+        return self.amount_cards < 15            
+
+
+def clear_desk():
+    os.system('cls' if os.name == 'nt' else 'clear')
--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 #main
 from logging import raiseExceptions
-from Croupier import croupier
 from Frame import console,income
 from Cards import cards
+import time
 
+
+#create config file
 start_test = "Welcome to my new game!\nPlease, write income down"
 incorrect_input = "You write incorrect value. Try again!"
-choise_input_text = "You gotta choose next step (Stop or take)\n"
+choise_input_text = "You gotta choose next step (stop or take)\n"
 stop_word = "stop"
 take_word = "take"
 income_human = income.Income(0)
 income_bot = income.Income(0)
-new_consolee = console.Desk()
 is_classic_game = True
 desk_cards = cards.DeskOfCards()
 
+
+
 def preparation():
     try:
+        #search bug in the income class
         money = int(input())
         income_human.set_income(money)
         income_bot.set_income(money)
@@ -33,42 +37,70 @@ def preparation():
         preparation()
         
 
+#remove in another lolation
 def game():
+    humon_stop_step = False
+    game = True
     try:
-        humon_stop_step = False
+        new_human_desk = console.Desk()
+        new_bot_desk = console.Desk()  
         desk_cards.create_cards(is_classic_game)
-        while(True):
+        while(game):
             if not humon_stop_step:
+                #take another method
                 choice_humon = str(input(choise_input_text))
                 if choice_humon.lower() == take_word:
-                    new_consolee.clear_desk()
-                    new_consolee.add_card(desk_cards.get_card())
-                    print(new_consolee.print_all_cards())
+                    console.clear_desk()
+                    new_human_desk.add_card(desk_cards.get_card())
+                    print(new_human_desk.get_all_cards("Your"))
                 elif choice_humon.lower() == stop_word:
+                    is_lose = new_human_desk.has_too_many_cards()
                     humon_stop_step = True
                 else:
                     print(incorrect_input)
                     continue
             else:
-                pass
-                #I gotta write croupier bot code
+                time.sleep(1)
+                console.clear_desk()
+                new_bot_desk.add_card(desk_cards.get_card())
+                print(new_bot_desk.get_all_cards("Bot"))
+                game = new_bot_desk.is_enouth()
+                is_lose = new_bot_desk.has_too_many_cards()
+        print("game over!!!")
+        #If u win return true
+        return True    
     except Exception as e:
         print(e)
 
 
 def main():
+    count_game = 0
     try:
         print(start_test)
         preparation()
         while(True):
-            game()
+            count_game += 1  
+            if game():
+                #make another method for this
+                income_human.set_income(income_human.get_income() + 1)
+                income_bot.set_income(income_bot.get_income() - 1)
+                print("You win game - " + str(count_game) +\
+                     "\nYour income - " + str(income_human.get_income()))
+            else:
+                income_human.set_income(income_human.get_income() - 1)
+                income_bot.set_income(income_bot.get_income + 1)
+                print("You lose game - " + str(count_game) +\
+                     "\nYour income - " + str(income_human.get_income()))  
+
             if income_human.get_income() == 0:
                 print("Bot win!")
             elif income_bot.get_income() == 0:
-                print("You win!")   
-
+                print("You win!")  
+            time.sleep(4)   
+            console.clear_desk()     
+            print("Next round")
     except Exception as e:
-        print(e)
+        print(e.with_traceback())
 
 
 if __name__ == "__main__":
